6ec999650a9295c8fa1766b9a67de3d7a4473a3c
Sam <teamholio@gmail.com>
2016-01-05 22:47:41 +0100

    Update projet.h
    Ajouté le type de logement dans la structure logement.

--- projet.h
+++ projet.h
@@ -4,25 +4,19 @@
 
 typedef struct  //Constitue la structure de l'élément Cité.
 {
-    char nom[30];   //Nom de la cité (ex: Dollet).
+    char nom[30];   //Nom de la cité (ex: Dolet).
     char adresse[30];   //Adresse de la cité (ex: 10 Boulevard Paul Locher Lagaye).
     char ville[30];    //Ville de la cité (ex: Aubière).
     int ref;   //Référence de la cité (ex: 100).
 	int nblogement //Nombre de logements dont la cité dispose.
 } cite;
 
-typedef struct
-{
-	char bat; //Référence du batiment.
-	int etage; //Référence l'étage.
-	int numero; //Référence le numéro de la chambre.
-}chambre;
-
 typedef struct  //Constitue la structure de l'élément Logement.
 {
     char ref[4];    //Référence du logement (Première lettre donne la cité, le chiffre suivant l'étage et les 2 derniers le numéro de la chambre).
     int handicap;    //Permet de savoir si une chambre permet d'accueillir un handicapé (ex: 1 pour Oui et 0 pour Non).
     int etat;   //Permet de savoir l'état d'une chambre (ex: 0 pour Libre, 1 pour Occupé et 2 pour En Travaux).
+	char type[2];   //Type de logement demandé (ex: C pour Chambre, S pour Studio T1 et T2)
 } logement;
 
 typedef struct  //Constitue la structure de l'élément date que l'on utilise dans la structure de Etudiant.
