3859f7d05c62c9e5c8afd4927e2a1c129c197b3f
Sam <teamholio@gmail.com>
2016-01-06 23:07:54 +0100

    Update projet.c
    J'ai un problème dans la lecture, il ne lit pas correctement la deuxième cité...

--- projet.c
+++ projet.c
@@ -7,27 +7,27 @@ fgets(c.nom,30,flot);
 c.nom[strlen(c.nom)-1]='\0';
 fgets(c.adresse,30,flot);
 c.adresse[strlen(c.adresse)-1]='\0';
-fscanf(flot,"%d %d %d %d %d",c.refcite,c.chambre,c.studio,c.T1,c.T2);
+fscanf(flot,"%d %d %d %d %d",&c.refcite,&c.chambre,&c.studio,&c.T1,&c.T2);
 return c;
 }
 
-int fchargementcite(char *nomfichier,Cite *cite[])
+int chargefcite(char *nomfichier,Cite *tcite[],int tmax)
 {
-int n,i;
+int n=0;
 Cite c;
 FILE *fe;
 fe=fopen(nomfichier,"r");
-if(fe==NULL) {printf("\n Pb ouverture fichier cite.don"); exit(1);}
-fscanf(fe,"%d",&n);
-for (i=0;i<n;i++)
+if(fe==NULL) {printf("\n Pb ouverture fichier cite.don"); return -1;}
+c=lirecite(fe);
+while (feof(fe)==0)
 	{
-    c=lirecite(fe);
-    cite[i]=(Cite*)malloc(sizeof(Cite));
-    if(cite==NULL){printf("Probleme malloc");return -1;}
-	*cite[i]=c;
+    tcite[n]=(Cite*)malloc(sizeof(Cite));
+    if(tcite==NULL){printf("\n Probleme malloc");return -1;}
+	*tcite[n]=c;
+	c=lirecite(fe);
+	n++;
 	}
 fclose(fe);
-printf("\n %d \n",n);
 return n;
 }
 
@@ -36,14 +36,14 @@ void affichecite(Cite **cite,int nbcite)
 int i;
 for(i=0;i<nbcite;i++)
     {
-    printf("\n %s \n %s \n %d \n %d \n %d \n %d \n %d \n",cite[i]->nom,cite[i]->adresse,cite[i]->refcite,cite[i]->chambre,cite[i]->studio,cite[i]->T1,cite[i]->T2);
+    printf(" %s \n %s \n %d \n %d \n %d \n %d \n %d \n",cite[i]->nom,cite[i]->adresse,cite[i]->refcite,cite[i]->chambre,cite[i]->studio,cite[i]->T1,cite[i]->T2);
     }
 }
 void global(void)
 {
-int nbat;
-Cite **tabresidence;
-nbat=fchargementcite("cite.don",tabresidence);
-affichecite(tabresidence,nbat);
+int nbcite;
+Cite *tresidence[100];
+nbcite=chargefcite("cite.don",tresidence,100);
+affichecite(tresidence,nbcite);
 return;
 }
