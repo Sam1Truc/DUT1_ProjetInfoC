c726395adb4d7017bfb9b5f0e0908da3d853183d
Sam <teamholio@gmail.com>
2016-01-06 22:12:31 +0100

    Update projet.h

--- projet.h
+++ projet.h
@@ -6,40 +6,34 @@ typedef struct  //Constitue la structure de l'élément Cité.
 {
     char nom[30];   //Nom de la cité (ex: Dolet).
     char adresse[30];   //Adresse de la cité (ex: 10 Boulevard Paul Locher Lagaye).
-    char ville[30];    //Ville de la cité (ex: Aubière).
-    int ref;   //Référence de la cité (ex: 100).
-	int nblogement //Nombre de logements dont la cité dispose.
-} cite;
+    int refcite;   //Référence de la cité (ex: 100).
+	int chambre; //Nombre de chambre dont la cité dispose.
+	int studio; //Nombre de studio dont la cité dispose.
+	int T1; //Nombre de studio dont la cité dispose.
+	int T2; //Nombre de studio dont la cité dispose.
+} Cite;
 
 typedef struct  //Constitue la structure de l'élément Logement.
 {
     char ref[4];    //Référence du logement (Première lettre donne la cité, le chiffre suivant l'étage et les 2 derniers le numéro de la chambre).
     int handicap;    //Permet de savoir si une chambre permet d'accueillir un handicapé (ex: 1 pour Oui et 0 pour Non).
     int etat;   //Permet de savoir l'état d'une chambre (ex: 0 pour Libre, 1 pour Occupé et 2 pour En Travaux).
-	char type[2];   //Type de logement demandé (ex: C pour Chambre, S pour Studio T1 et T2)
-} logement;
-
-typedef struct  //Constitue la structure de l'élément date que l'on utilise dans la structure de Etudiant.
-{
-    int jour;   //Jour de naissance (ex: 03).
-    int mois;   //Mois de naissance (ex: 01).
-    int annee;  //Annee de naissance (ex: 1997).
-} date;
+	char type[2];   //Type de logement demandé (ex: C pour Chambre, S pour Studio T1 et T2).
+} Logement;
 
 typedef struct  //Constitue la structure de l'élément Etudiant.
 {
+	int refetud; //Numéro d'étudiant.
     char nom[20];   //Nom de l'étudiant (ex: Dupont).
     char prenom[20];    //Prénom de l'étudiant (ex: Arthur).
-    date naissance; //est constitué de l'élement Date (ex: 03/01/1997).
-    int bourse; //Echelon de la bourse de l'étudiant (De 1 à 7 ou 0 si l'étudiant n'a pas le droit aux bourses).
+    int bourse; //Echelon de la bourse de l'étudiant (De 0 à 7 ou -1 si l'étudiant n'a pas le droit aux bourses).
     int handicap;  //Permet de savoir si un étudiant est handicpé ou non (ex: 1 pour Oui et 0 pour Non).
-    char etude[30];    //Permet de savoir quelles études réalisent l'étudiant (ex: DUT Informatique).
     char refchamb[4];  // Référence de la chambre qu'occupe l'étudiant (ex: A001 ou 0 s'il n'a pas encore de chambre).
-} etudiant;
+} Etudiant;
 
 typedef struct  //Constitue la structure de l'élément Demande.
 {
-    etudiant etud;   //est constitué de la structure étudiant.
-    char refcite[20];   //Référence de la cité demandée par l'étudiant (ex: Dolet).
-    char type[2];   //Type de logement demandé (ex: C pour Chambre, S pour Studio T1 et T2).
-} demande;
+    Etudiant etud;   //est constitué de la structure étudiant.
+    int refcite;   //Référence de la cité demandée par l'étudiant (ex: 1).
+    char type[2];   //Type de logement demandé (ex: C pour Chambre, S pour Studio, T1 et T2).
+} Demande;
